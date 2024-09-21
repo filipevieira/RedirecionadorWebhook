25b923e3c97f86bdb98ea64a65ecfe01ef15114a
Filipe Vieira <filipevieiradossantos@gmail.com>
2024-09-21 02:09:22 -0300

    🚧 Refatora a função handle_webhook para iniciar a aplicação Flask na porta 8000

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
 from WebhookRediGlobal import app  # Importe sua aplicação Flask
 
 if __name__ == "__main__":
-    app.run()  # Inicia a aplicação Flask (não necessário em produção)
+    app.run(port=8000)  # Inicia a aplicação Flask (não necessário em produção)
